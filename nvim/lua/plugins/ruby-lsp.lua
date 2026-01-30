159be18e9bc45e576f27d3eda2cc6ce4dacc944d
Marivaldo Cavalheiro <marivaldo@gmail.com>
2026-01-30 20:21:36 -0300

    Add solargraph fallback for Ruby 2.7 projects and update mason repo

--- nvim/lua/plugins/ruby-lsp.lua
+++ nvim/lua/plugins/ruby-lsp.lua
@@ -1,10 +1,42 @@
 return {
   {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "ruby-lsp",
+      },
+    },
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      -- Don't auto-install solargraph via Mason (use system version)
+      automatic_installation = {
+        exclude = { "solargraph" },
+      },
+    },
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         ruby_lsp = {
           cmd = { vim.fn.expand("~/.local/bin/ruby-lsp-wrapper") },
+        },
+        solargraph = {
+          -- Use system solargraph (installed via Homebrew)
+          mason = false,
+          cmd = { "solargraph", "stdio" },
+          settings = {
+            solargraph = {
+              diagnostics = true,
+              completion = true,
+              hover = true,
+              references = true,
+              rename = true,
+              symbols = true,
+            },
+          },
         },
       },
     },
